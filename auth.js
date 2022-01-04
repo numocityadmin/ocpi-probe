5238dbf8e0a895f76fc5027875e97f2fe75337ac
Lohith <lohith.p@numocity.com>
2022-01-04 16:54:53 +0530

    auth change

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ const {fetchToken}=require('./tokens');
 
 async function checkAuth(authorization) {
   const tokenB=authorization.split(' ');
-  const tokenRecord=await fetchToken('TokenB');
+  const tokenRecord=await fetchToken('tokenCWithEndpoints');
   if (tokenRecord.token==tokenB[1]) {
     return true;
   } else {
